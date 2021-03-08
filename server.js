82bd29f084486d493f91bc070df004eb4b51af16
Dilan Silva <sdilansilva@gmail.com>
2021-03-08 16:17:19 +0530

    Make routes for crud operation

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');//import express
+const app = express();
+const port = 3000;
+
+app.post('/create',(req,res) => {
+
+});
+
+app.get('/read',(req,res) => {
+
+});
+
+app.put('/update', (req, res) => {
+    
+});
+
+app.delete('/delete', (req, res) => {
+    
+});
+
+app.listen(port, () => {
+    console.log('App listening on port 3000!');
+});
