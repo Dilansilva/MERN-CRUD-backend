296aa52738e4a1bf40f3e14196c43cf7202e1bc2
Dilan Silva <sdilansilva@gmail.com>
2021-03-08 20:02:09 +0530

    Routes are done

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const app = express();
 const port = 3000;
 
 app.post('/create',(req,res) => {
-
+    console.log('create');
 });
 
 app.get('/read',(req,res) => {
-
+    console.log('read');
 });
 
 app.put('/update', (req, res) => {
-    
+    console.log('update');
 });
 
 app.delete('/delete', (req, res) => {
-    
+    console.log('delete');
 });
 
 app.listen(port, () => {
